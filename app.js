7ae72295722ad5f454fb47dd0c44911f8bd9ba54
sostene <ngarukiyimanasostene@gmail.com>
2022-03-31 21:52:31 +0200

    node js, express requests and response a swell as ejs

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+
+// setting up an express app
+const app = express();
+
+// register view engine
+app.set("view engine", "ejs");
+
+// listen for requests
+app.listen(3000);
+
+app.get("/", (req, res) => {
+  // res.send("<h3>Home page</h3>");
+  res.sendFile("./views/index.html", { root: __dirname });
+});
+
+app.get("/about", (req, res) => {
+  // res.send("<h3>About page</h3>");
+  res.sendFile("./views/about.html", { root: __dirname });
+});
+
+// redirects
+
+app.get("/about-us", (req, res) => {
+  res.redirect("/about");
+});
+
+// 404 page
+app.use((req, res) => {
+  res.status(404).sendFile("./views/404.html", { root: __dirname });
+});
