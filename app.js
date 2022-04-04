70bea70d6690ef6cffa17f11ad0da8f22b870277
sostene <ngarukiyimanasostene@gmail.com>
2022-04-04 23:58:50 +0200

    working on mango db , mongoose and schema also static files like css and images

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
 const express = require("express");
 const morgan = require("morgan");
+const mongoose = require("mongoose");
 
 // setting up an express app
 const app = express();
+
+//connect to mongodb
+const db =
+  "mongodb+srv://sostene:CaGUc402jkLlf1LJ@cluster0.tvlir.mongodb.net/node-js?retryWrites=true&w=majority";
+
+mongoose
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("the connection is complete");
+    // listen port
+    const port = 3000;
+    app.listen(port, () => {
+      console.log("app is listening on port " + port);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 // register view
 app.set("view engine", "ejs");
@@ -10,14 +29,11 @@ app.set("view engine", "ejs");
 // by default ejs looks in your directory and search for folde called views
 // app.set("views","my views") ; //setting up  another folder if necessary
 
-// listen for
-const port = 3000;
-app.listen(port, () => {
-  console.log("app is listening on port " + port);
-});
-
 // middleware
-app.use(morgan("common"));
+app.use(morgan("dev"));
+
+// middleware and ststic files
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   // res.sendFile("./views/index.html", { root: __dirname });
